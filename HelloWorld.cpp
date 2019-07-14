4e61874b47e2558e80560a53630c6c17e6b730ea
sheng liwei <shengliwei1019@gmail.com>
2019-07-14 11:12:57 +0800

    Add hello world script by c++

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+// my first program in C++
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i;
+  i=5;
+  char str[]={"Hello World!"};
+  cout<<i<<endl<<str<<endl;
+  
+  //std::cout << "Hello World!"
+  //	    << std::endl;
+}
